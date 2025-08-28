e8d27d53c7265478674c1b69b00f6606b1732a24
Stainman Hu <stanleyhuofficial@gmail.com>
2025-08-27 21:39:58 -0700

    better logic

--- players.py
+++ players.py
@@ -16,7 +16,7 @@ class Human:
         if(inp=="show"):
             return (None, "********")
         #//////////////////////////////////////////////////////////////////////////////////////////////////////////
-        play = playCards(self.hand, inp)
+        play = encodeSelection(self.hand, inp)
         while ((table and not canPlay(play, table[CARD], table[CARD_TYPE], table[TRASH_TYPE])) or not play):
             inp = input("INVALID card! Try again: ")
             if(inp=='' and table):
@@ -27,7 +27,7 @@ class Human:
             elif(inp=="show"):
                 return (None, "********")
             #//////////////////////////////////////////////////////////////////////////////////////////////////////
-            play = playCards(self.hand, inp)
+            play = encodeSelection(self.hand, inp)
             
         message = ''
         for card in self.hand.get_list(inp.split(" "), limit=1, sort=True, ranks=new_ranks):
@@ -51,19 +51,41 @@ class SimpleAI:
 
     def select(self, table):
         if not table: 
+            table_cards, table_type, table_trash = ['Null'], None, None
+        else:
+            table_cards, table_type, table_trash = table[CARDS], table[CARD_TYPE], table[TRASH_TYPE]
+            
+        play = np.random.choice(getAllPlays(self.hand, table_cards, table_type))
+        if('aaa' in play.card_type and not table): 
+            play.trash = (rd.choice['', 'a', 'aa'], int(len(play.card_type)/3))
+        removed = removeCards(self.hand, play)
+        if not removed: 
+            print(f"{self.name} has skipped turn")
+            return None
+        print(f"{self.name} has played {removed}")
+        return play
+        
+        if table_trash: play.  getTrash(self.hand, table_trash, play)
+        
+        if not play:
+            print(f"{self.name} has skipped turn")
+            return None
+        
+        
+        
+        if not table: 
             card, type, trash = None, None, None
         else:
             card, type, trash = table[CARD], table[CARD_TYPE], table[TRASH_TYPE]
         combo = getPlay(self.hand, card, type, trash, getComboList(self.hand))
         play = playCards(self.hand, combo)
-        if(not play):
-            print(f"{self.name} has skipped turn")
-            return play
+        
         message = ''
         for card in self.hand.get_list(combo.split(" "), limit=1, sort=True, ranks=new_ranks):
             message+=f"{card}, "
         print(f"{self.name} has played {message}")
         return play
+    
 
 
     def getEmpty(self):
@@ -72,8 +94,8 @@ class SimpleAI:
     def __str__(self):
         return f"\n{self.name} (simpleAI)\n{self.hand}\n"
     
-class SmartAI(SimpleAI):
-    def __init__(self, name):
-        super().__init__(name)
+# class SmartAI(SimpleAI):
+#     def __init__(self, name):
+#         super().__init__(name)
 
-    def select(self, table):
+#     def select(self, table):
--- utils.py
+++ utils.py
@@ -60,6 +60,15 @@ def prevVal(value):
         return ranks[ranks.index(value)-1]
     except (ValueError, IndexError):
         return None
+    
+
+def canPlay(play, table):
+    if(play):
+        if(not table.cards or not table.card_type): return True
+        if(play.card_type=='aaaa' and table.card_type!='aaaa'): return True
+        if(play.card_type==table.card_type and play>table and play.trash==table.trash): return True
+        if(play.card_type=='aaaa' and table.card_type!='aaaa'): return True
+    return False
 
 
 def maxDupes(cards):
@@ -80,34 +89,34 @@ def maxDupes(cards):
     return (maxDupe, dupeCard, numDupes)
         
     
-def encodeCards(hand, choice):
-    list = [c for c in choice if c]
+def encodeSelection(hand, selection):
+    list = [c for c in selection if c]
     copy = pd.Stack(cards=hand, sort=True, ranks=new_ranks)
     cards = pd.Stack(cards=copy.get_list(list, limit=1), sort=True, ranks=new_ranks)
     if(cards.size!=len(list)): 
         return None
-    trash = None
+    trash = ('', 0)
     maxDupe, dupeCard, numDupes = maxDupes(cards)
     
     if(maxDupe==1):
         combo = 'a'
-        if(cards.size==1): return Play(choice, combo, trash)
+        if(cards.size==1): return Play(selection, combo, trash)
         if(cards.size>=5 and not len(cards.find('Joker'))):
             for i in range(cards.size-1):
                 if(not isConsecutive(cards[i], cards[i+1])):
                     return None
                 combo+=chr(ord(combo[-1]) + 1)
-            return Play(choice, combo, trash)
+            return Play(selection, combo, trash)
     
     if(maxDupe==2):
         combo = 'aa'
-        if(cards.size==2): return (choice, combo, trash)
+        if(cards.size==2): return (selection, combo, trash)
         if(cards.size>=6 and cards.size%numDupes==0 and not len(cards.find('Joker'))):
             for i in range(int(1.0*cards.size/2)-1):
                 if(not isConsecutive(cards[i*2], cards[(i+1)*2])):
                     return None
                 combo+=chr(ord(combo[-1]) + 1)*2
-            return Play(choice, combo, trash)
+            return Play(selection, combo, trash)
     
     if(maxDupe==3 and not len(cards.find('Joker'))):
         combo = 'aaa'
@@ -119,55 +128,55 @@ def encodeCards(hand, choice):
             if(not isConsecutive(tripples[-1], tripples[-2])): return None
             combo+=chr(ord(combo[-1]) + 1)*3
         maxDupe, _, numTrash = maxDupes(cards)
-        if(numTrash==len(tripples)): return Play(choice, combo, ('a'*maxDupe, numTrash))
-        if(not numTrash): return Play(choice, combo, trash)
+        if(numTrash==len(tripples)): return Play(selection, combo, ('a'*maxDupe, numTrash))
+        if(not numTrash): return Play(selection, combo, trash)
     
     if((maxDupe==4 and cards.size==4) or (maxDupe==1 and cards.size==2 and dupeCard.value=='Small')):
-        return Play(choice, 'aaaa', trash)
+        return Play(selection, 'aaaa', trash)
     return None
 
 
 
 
-def getMoves(hand, value, table_type):
+def getCardPlays(hand, card, table_type):
     combos = np.array([])
     cap = new_ranks['values']['2']
-    combo = np.array([value])
-    for size in range(1, len(hand.find(value))+1):
-        move = encodeCards(hand, combo)
-        if(move[1]==table_type): combos = np.append(combos, move)
+    combo = card.value
+    for size in range(1, len(hand.find(card.value))+1):
+        move = encodeSelection(hand, combo)
+        if(move.card_type==table_type): combos = np.append(combos, move)
         if(size==1):
-            next = nextVal(value)
+            next = nextVal(card)
             chain = combo
             count = 1
             while(next and hand.find(next) and new_ranks['values'][next]<cap):
                 count+=1
                 chain = np.append(chain, next)
                 if(count>=5):
-                    move = encodeCards(hand, chain)
+                    move = encodeSelection(hand, chain)
                     if(move[1]==table_type): combos = np.append(combos, move)
                 next = nextVal(next)
         elif(size==2):
-            next = nextVal(value)
+            next = nextVal(card)
             chain = combo
             count = 1
             while(next and hand.find(next) and new_ranks['values'][next]<cap and len(hand.find(next))>=2):
                 count+=1
                 chain = np.append(chain, [next, next])
                 if(count>=3):
-                    move = encodeCards(hand, chain)
+                    move = encodeSelection(hand, chain)
                     if(move[1]==table_type): combos = np.append(combos, move)
                 next = nextVal(next)
         elif(size==3):
-            next = nextVal(value)
+            next = nextVal(card)
             chain = combo
             while(next and hand.find(next) and new_ranks['values'][next]<cap and len(hand.find(next))>=3):
                 chain = np.append(chain, [next, next, next])
-                move = encodeCards(hand, chain)
+                move = encodeSelection(hand, chain)
                 if(move[1]==table_type): combos = np.append(combos, move)
                 next = nextVal(next)
                 
-        combo = np.append(combo, value)
+        combo+=card.value
     return combos
 
 
@@ -182,77 +191,79 @@ def getMoves(hand, value, table_type):
 #     return comboList
 
 
-def getTrash(hand, combo, trash, comboList):
-    totalTrash = ''
-    if(trash==''): return totalTrash
-    decode = trash.split(' ')
-    dupe, num = decode[0], int(decode[1])
-    copy = pd.Stack(cards=hand, sort=True, ranks=new_ranks)
-    copy.get_list(combo.split(" "), limit=1, sort=True, ranks=new_ranks)
-    if(dupe and num):
-        getTrash = getPlay(copy, NULL, dupe, '', comboList)
-        copy.get_list(getTrash.split(" "), limit=1, sort=True, ranks=new_ranks)
-        totalTrash+=getTrash
-        for _ in range(num-1):
-            getTrash = getPlay(copy, NULL, dupe, '', comboList)
-            if(getTrash==''): return None
-            copy.get_list(getTrash.split(" "), limit=1, sort=True, ranks=new_ranks)
-            totalTrash+=' '+getTrash
-    return totalTrash
+# def getTrash(hand, combo, trash, comboList):
+#     totalTrash = ''
+#     if(trash==''): return totalTrash
+#     decode = trash.split(' ')
+#     dupe, num = decode[0], int(decode[1])
+#     copy = pd.Stack(cards=hand, sort=True, ranks=new_ranks)
+#     copy.get_list(combo.split(" "), limit=1, sort=True, ranks=new_ranks)
+#     if(dupe and num):
+#         getTrash = getPlay(copy, NULL, dupe, '', comboList)
+#         copy.get_list(getTrash.split(" "), limit=1, sort=True, ranks=new_ranks)
+#         totalTrash+=getTrash
+#         for _ in range(num-1):
+#             getTrash = getPlay(copy, NULL, dupe, '', comboList)
+#             if(getTrash==''): return None
+#             copy.get_list(getTrash.split(" "), limit=1, sort=True, ranks=new_ranks)
+#             totalTrash+=' '+getTrash
+#     return totalTrash
 
 
-def getPlay(hand, card, type, trash, comboList):
-    if(not card and not type and not trash): 
-        combo = max(comboList[hand[0].value], key=len)
-        play = encodeCards(hand, combo)
-        if('aaa' in play[CARD_TYPE] and play[CARD_TYPE]!='aaaa'):
-            choice = rd.choice(['a', 'aa'])+' '+str(int(len(play[CARD_TYPE])/3))
-            newCombo = combo+' '+getTrash(hand, combo, choice, comboList)
-            if(encodeCards(hand, newCombo)): return newCombo
-        return combo
+# def getPlay(hand, card, type, trash, comboList):
+#     if(not card and not type and not trash): 
+#         combo = max(comboList[hand[0].value], key=len)
+#         play = encodeCards(hand, combo)
+#         if('aaa' in play[CARD_TYPE] and play[CARD_TYPE]!='aaaa'):
+#             choice = rd.choice(['a', 'aa'])+' '+str(int(len(play[CARD_TYPE])/3))
+#             newCombo = combo+' '+getTrash(hand, combo, choice, comboList)
+#             if(encodeCards(hand, newCombo)): return newCombo
+#         return combo
     
-    for curr in hand:
-        if(curr.gt(card, new_ranks)):
-            for combo in comboList[curr.value]:
-                if(canPlay(encodeCards(hand, combo), card, type, '')):
-                    return combo+' '+getTrash(hand, combo, trash, comboList)
+#     for curr in hand:
+#         if(curr.gt(card, new_ranks)):
+#             for combo in comboList[curr.value]:
+#                 if(canPlay(encodeCards(hand, combo), card, type, '')):
+#                     return combo+' '+getTrash(hand, combo, trash, comboList)
                 
-    if(type!='aaaa'): return getPlay(hand, NULL, 'aaaa', '', comboList)
-    return ''
+#     if(type!='aaaa'): return getPlay(hand, NULL, 'aaaa', '', comboList)
+#     return ''
 
 
-def getPlay(hand, table_cards, table_type, table_trash):
-    possiblePlays = getPossiblePlays(hand, table_cards, table_type)
-    trash = Play()
-    play = np.random.choice(possiblePlays)
-    if(table_trash):
-        copy = pd.Stack(cards=hand, sort=True, ranks=new_ranks)
-        copy.get_list(play[CARDS], limit=1, sort=True, ranks=new_ranks)
-        possibleTrash = getPossiblePlays(copy, 'Null', table_trash[DUPE_TRASH], '')
-        for _ in range(table_trash[NUM_TRASH]):
-            idx = np.random.randint(len(possibleTrash))
-            trash = possibleTrash[idx]
-            possibleTrash = np.delete(possibleTrash, idx)
-            play = play + trash
-    return play
-
-
-def getPossiblePlays(hand, table_cards, table_type):
-    possibleMoves = np.array([])
-    if(hand.size==0): return possibleMoves
+def getAllPlays(hand, table_cards, table_type):
+    plays = np.array([])
+    if(hand.size==0): return plays
     prev = None
     for card in hand:
-        if(new_ranks[card.value]>new_ranks[table_cards.split(' ')[0]] and prev.value!=card.value):
-            possibleMoves = possibleMoves.concantenate(getMoves(hand, card, table_type))
+        if(not table_cards or (new_ranks['values'][card.value]>new_ranks['values'][table_cards[0]] and (not prev or prev.value!=card.value))):
+            plays = np.concatenate(plays, getCardPlays(hand, card, table_type))
         prev = card
-    return possibleMoves
+    return plays
+
+def getTrash(hand, play):
+    copy = pd.Stack(cards=hand, sort=True, ranks=new_ranks)
+    copy.get_list(play.cards, limit=1, sort=True, ranks=new_ranks)
+    possibleTrash = getAllPlays(copy, ['Null'], play.trash[0])
+    trash = Play()
+    for _ in range(play.trash[1]):
+        idx = np.random.randint(len(possibleTrash))
+        trash+=possibleTrash[idx]
+        possibleTrash = np.delete(possibleTrash, idx)
+    return trash
+
+
+def removeCards(hand, play):
+    if not play or not hand: return None
+    trash = getTrash(hand, play)
+    return np.concatenate(np.array(hand.get_list(play.cards, limit=1, sort=True, ranks=new_ranks)), 
+                          (np.array(hand.get_list(trash.cards, limit=1, sort=True, ranks=new_ranks))))
 
 
 class Play:
-    def __init__(self, cards=np.array([]), card_type='Null', trash_type=None):
-        self.cards = cards
+    def __init__(self, cards=np.array([]), card_type=None, trash=None):
+        self.cards = np.array(cards)
         self.card_type = card_type
-        self.trash_type = trash_type
+        self.trash = trash
         
     def __gt__(self, other, ranks):
         if not other: return True
@@ -263,7 +274,14 @@ class Play:
     def __add__(self, other):
         if not other:
             return self.cards
-        return self.cards.concantenate(other.cards)
+        return np.concatenate(self.cards, other.cards)
 
     def __str__(self):
         return f"{self.cards} {self.card_type} {self.trash_type}"
+    
+    # def __eq__(self, other):
+    #     if len(self.cards)!=len(other.cards): return False
+    #     if self.card_type!=other.card_type: return False
+    #     if self.rtash!=other.trash: return False
+    #     for i in range(len(self.cards)):
+    #         if self.cards[i]!=other.cards[i]: return False
